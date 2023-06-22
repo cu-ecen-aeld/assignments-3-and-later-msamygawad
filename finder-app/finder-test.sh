0c55ccfd8ec5ca83cfe34493fb53fc3a2200427b
msamygawad <msamygawad@gmail.com>
2023-06-22 05:34:53 +0300

    edited finder-test.sh for assignment-4-part2 and modified make command in finder-app dir

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,8 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+CONFIG_DIR=/etc/finder-app/conf
+username=$(cat ${CONFIG_DIR}/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -33,7 +34,7 @@ rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
 
-assignment=`cat ./conf/assignment.txt`
+assignment=`cat ${CONFIG_DIR}/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -55,10 +56,12 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(sh ./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+
+echo ${OUTPUTSTRING} > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
